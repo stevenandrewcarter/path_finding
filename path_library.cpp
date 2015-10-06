051bac3ecdea1f3a7d1b5ac0692d2c6cad8050f4
Steven Carter <stonemason@gmail.com>
2015-10-06 16:39:22 +0200

    * More changes to the design

--- path_library.cpp
+++ path_library.cpp
@@ -1,8 +1,42 @@
 #include "path_library.h"
 #include <assert.h>
+#include <memory>
 
-int FindPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY, const unsigned char* pMap, const int nMapWidth, const int nMapHeight, int* pOutBuffer, const int nOutBufferSize)
-{
+namespace PathLibrary {
+  Graph::Graph(int a_width, int a_height, const unsigned char* a_data) {
+    assert(a_data != nullptr);
+    assert(a_width >= 1);
+    assert(a_height >= 1);
+    width_ = a_width;
+    height_ = a_height;
+    nodes_ = a_data;
+  }
+
+  int Graph::get_path(Node a_source, Node a_target) {
+    // Check simple case first
+    if (a_source == a_target)
+      return 0;
+    // Perform the search instead then
+    return 0;
+  }
+
+  std::vector<Node> Graph::get_edges(int a_x, int a_y) const {
+    assert(a_x >= 0 && a_x < width_);
+    assert(a_y >= 0 && a_y < height_);
+    std::vector<Node> edges;
+    auto current_node = nodes_[a_x * a_y];
+    if (current_node != '0') {
+      // Passible location, check the cardinal neighbours
+      if (a_x > 0 && nodes_[a_x - 1 * a_y] != '0') edges.push_back({ a_x - 1, a_y });
+      if (a_x < width_ && nodes_[a_x + 1 * a_y] != '0') edges.push_back({ a_x + 1, a_y });
+      if (a_y > 0 && nodes_[a_x * a_y - 1] != '0') edges.push_back({ a_x, a_y - 1 });
+      if (a_y < height_ && nodes_[a_x * a_y + 1] != '0') edges.push_back({ a_x, a_y + 1 });
+    }
+    return edges;
+  }
+}
+
+int FindPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY, const unsigned char* pMap, const int nMapWidth, const int nMapHeight, int* pOutBuffer, const int nOutBufferSize) {
   // Perform upfront sanity checks  
   assert(nStartX >= 0 && nStartX <= nMapWidth);
   assert(nStartY >= 0 && nStartY <= nMapHeight);
@@ -10,16 +44,6 @@ int FindPath(const int nStartX, const int nStartY, const int nTargetX, const int
   assert(nTargetY >= 0 && nTargetY <= nMapHeight);
   assert(nOutBufferSize > 0);
   // Construct the grid as a graph
-  return 0;
-}
-
-Graph::Graph(int a_width, int a_height, const unsigned char* a_data)
-{
-  // Perform sanity checks
-  assert(a_data != nullptr);
-  assert(a_width >= 1);
-  assert(a_height >= 1);
-  width_ = a_width;
-  height_ = a_height;
-  // Build the Graph from the data
+  auto graph = std::make_unique<PathLibrary::Graph>(nMapWidth, nMapHeight, pMap);
+  return graph->get_path({ nStartX, nStartY }, { nTargetX, nTargetY });
 }
--- path_library.h
+++ path_library.h
@@ -1,59 +1,41 @@
 #pragma once
 #include <vector>
-#include <memory>
 
-// Predeclare classes
-class Edge;
+namespace PathLibrary {
+  struct Node {
+    int x, y;
 
-/**
-* Node describes a intersection between graph edges
-* For the A* path finding implementation this doesn't really do much, but could include
-* important information for future reasons.
-*/
-class Node
-{
-public:
-  Node(int a_x, int a_y);
+    bool operator ==(const Node &b) const {
+      return x == b.x && y == b.y;
+    }
+  };
 
-private:
-  std::vector<std::shared_ptr<Edge>> edges_;
-  int x_, y_;
-};
-
-/**
- * A edge represents a possible connection between two nodes in a graph. Each edge should be unique and non circular.
- */
-class Edge
-{
-public:
-  Edge(Node* a_source, Node* a_destination);
-
-private:
-  Node *source_, *destination_;
-};
-
-/**
- * Graph object describes a graph of nodes and edges. The Graph is a 2D grid.
- * Some important properties of the Graph
- *  1. The graph edges can only be defined horizontally or vertically (No edge can be diagonal)
- *  2. The origin position of the Graph is (0, 0) with the x and y values increasing into the positive plane.
- */
-class Graph
-{
-public:
   /**
-   * Default constructor for a Graph. The Graph is defined by the width and height values provided
-   * @param a_width Width of the graph (X Axis)
-   * @param a_height Height of the graph (Y Axis)
-   * @param a_data 2D Data set that contains the data of the graph
+   * Graph object describes a graph of nodes and edges. The Graph is a 2D grid.
+   * Some important properties of the Graph
+   *  1. The graph edges can only be defined horizontally or vertically (No edge can be diagonal)
+   *  2. The origin position of the Graph is (0, 0) with the x and y values increasing into the positive plane.
    */
-  Graph(int a_width, int a_height, const unsigned char* a_data);
-  ~Graph();
+  class Graph {
+  public:
+    /**
+     * Default constructor for a Graph. The Graph is defined by the width and height values provided
+     * @param a_width Width of the graph (X Axis)
+     * @param a_height Height of the graph (Y Axis)
+     * @param a_data 2D Data set that contains the data of the graph
+     */
+    Graph(int a_width, int a_height, const unsigned char* a_data);
+    ~Graph() {};
 
-private:
-  std::vector<std::shared_ptr<Node>> nodes_;
-  int width_, height_;
-};
+    int get_path(Node a_source, Node a_target);
+
+  private:
+    std::vector<Node> get_edges(int a_x, int a_y) const;
+
+    int width_, height_;
+    const unsigned char* nodes_;
+  };
+}
 
 /**
  * Find the shortest path between the Start Position and the Target Position
